a8d5253c8e8bf8960a8b58d82ba5072d204ccda3
okanfdlh <ignadeafiranti@gmail.com>
2026-02-01 15:00:09 +0700

    add dark mode

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+export default {
+  darkMode: ["class"], // ← WAJIB
+  content: [
+    "./app/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
